43e6233f81c6c05f421b2bcf6c8cc0d6197581b8
Lahari2335 <laharigudla306@gmail.com>
2025-10-20 16:59:50 +0530

    Update ExpenseController.java

--- EXPENSER-SPRINGBOOT/src/main/java/com/klef/dev/controller/ExpenseController.java
+++ EXPENSER-SPRINGBOOT/src/main/java/com/klef/dev/controller/ExpenseController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import com.klef.dev.entity.Expense;
 import com.klef.dev.service.ExpenseService;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/expenseapi")
 public class ExpenseController {
